c944c4f58d9c5cce5f5493c910a3896329749dc5
Mahima Chacko <mahimamchacko@gmail.com>
2024-12-07 20:56:32 -0500

    Add validaton

--- src/market.js
+++ src/market.js
@@ -6,6 +6,7 @@ import { authorize } from "./account.js";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import { addRoom, getRoom } from "./biddingrooms.js";
+import { error } from "console";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -82,7 +83,7 @@ router.get("/search", async (req, res) => {
       AND LOWER(product_name) like $1
       GROUP BY p.id, username, product_name, product_desc, start_time, closing_time, price;
     `,
-      [`%${name.toLowerCase()}%`],
+      [`%${name.toLowerCase()}%`]
     );
     products = result.rows;
     if (products.length === 0) {
@@ -125,7 +126,7 @@ router.get("/view/:id", authorize, async (req, res) => {
       WHERE p.id = $1
       GROUP BY p.id, u.username, product_name, product_desc, start_time, closing_time, price, w.username;
     `,
-      [id],
+      [id]
     );
 
     product = result.rows[0];
@@ -137,7 +138,7 @@ router.get("/view/:id", authorize, async (req, res) => {
       INNER JOIN tokens ON tokens.username = users.username
       WHERE token = $1;
       `,
-      [token],
+      [token]
     );
 
     userId = userResult.rows[0].id;
@@ -151,7 +152,7 @@ router.get("/view/:id", authorize, async (req, res) => {
   return res.render("market-view", {
     product: product,
     userId: userId,
-    room: getRoom(product.id),
+    room: product ? getRoom(product.id) : undefined,
     error: message,
   });
 });
@@ -167,7 +168,7 @@ router.get("/view/:id/:imageid", async (req, res) => {
       `
       SELECT image_name, image_data FROM images WHERE id = $1
     `,
-      [image_id],
+      [image_id]
     );
 
     if (result.rows.length === 1) {
@@ -189,6 +190,21 @@ router.get("/add", authorize, (req, res) => {
   return res.redirect("/account/dashboard/products/add");
 });
 
+let imageExtensions = [
+  ".jpg",
+  ".jpeg",
+  ".png",
+  ".gif",
+  ".bmp",
+  ".webp",
+  ".tiff",
+  ".svg",
+];
+function isImage(fileName) {
+  let ext = path.extname(fileName).toLowerCase();
+  return imageExtensions.includes(ext);
+}
+
 router.post("/add", authorize, upload.array("images"), async (req, res) => {
   let body = Object.create(Object.prototype);
   Object.assign(body, req.body);
@@ -200,48 +216,56 @@ router.post("/add", authorize, upload.array("images"), async (req, res) => {
   let product_closing_time = body["end"];
   let product_price = body["price"];
 
-  // TODO: validation
   let errors = [];
 
-  // Upload products into db
-  let user_id = req.params["user"];
-  let client = await pool.connect();
-  try {
-    await client.query("BEGIN");
-    let product_id = await client.query(
-      "INSERT INTO products (product_name, product_desc, seller_id, start_time, closing_time, price) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id;",
-      [
-        product_name,
-        product_desc,
-        user_id,
-        product_start_time,
-        product_closing_time,
-        product_price,
-      ],
-    );
-    product_id = product_id.rows[0]["id"];
-
-    for (let i = 0; i < files.length; i++) {
-      await client.query(
-        "INSERT INTO images (image_name, image_data, image_order, product_id) VALUES ($1, $2, $3, $4);",
-        [files[i].originalname, files[i].buffer, i + 1, product_id],
-      );
+  for (let i = 0; i < files.length; i++) {
+    let fileName = files[i].originalname;
+    if (!isImage(fileName)) {
+      errors.push(`${fileName} is not a valid image.`);
     }
+  }
 
-    await client.query("COMMIT");
+  if (errors.length === 0) {
+    // Upload products into db
+    let user_id = req.params["user"];
+    let client = await pool.connect();
+    try {
+      await client.query("BEGIN");
+      let product_id = await client.query(
+        "INSERT INTO products (product_name, product_desc, seller_id, start_time, closing_time, price) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id;",
+        [
+          product_name,
+          product_desc,
+          user_id,
+          product_start_time,
+          product_closing_time,
+          product_price,
+        ]
+      );
+      product_id = product_id.rows[0]["id"];
 
-    addRoom(
-      product_id,
-      product_price,
-      new Date(product_start_time),
-      new Date(product_closing_time),
-    );
-  } catch (error) {
-    console.log(error);
-    errors.push(error);
-    await client.query("ROLLBACK");
-  } finally {
-    client.release();
+      for (let i = 0; i < files.length; i++) {
+        await client.query(
+          "INSERT INTO images (image_name, image_data, image_order, product_id) VALUES ($1, $2, $3, $4);",
+          [files[i].originalname, files[i].buffer, i + 1, product_id]
+        );
+      }
+
+      await client.query("COMMIT");
+
+      addRoom(
+        product_id,
+        product_price,
+        new Date(product_start_time),
+        new Date(product_closing_time)
+      );
+    } catch (error) {
+      console.log(error);
+      errors.push(error);
+      await client.query("ROLLBACK");
+    } finally {
+      client.release();
+    }
   }
 
   if (errors.length === 0) {
